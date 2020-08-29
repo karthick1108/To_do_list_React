776c7fa1ee1663978f962dc6a181097af6cd2790
Karthick Rajasekaran <kraj0008@student.monash.edu>
2020-08-29 19:02:18 +1000

    removed unnecessary comments

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,7 @@
 import React from "react";
 
 const Form = ({ inputText, setInputText, todos, setTodos, setStatus }) => {
-  //function
   const inputTextHandler = (e) => {
-    console.log(e.target.value);
     setInputText(e.target.value);
   };
 
